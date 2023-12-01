676f5110938ed779fb5cab59eddc29273b74452e
ishizukuma <tky2202026@stu.o-hara.ac.jp>
2023-12-01 12:03:38 +0900

    ファイル_フォルダー構成整理

--- app_album/views.py
+++ app_album/views.py
@@ -3,4 +3,7 @@ from django.views import generic
 
 # Create your views here.
 class IndexView(generic.TemplateView):
-    template_name = "index.html"
+    template_name = "login.html"
+
+class TopView(generic.TemplateView):
+    template_name = "top.html"
