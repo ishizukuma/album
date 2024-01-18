59a9d2fa12b863cf610e6ef16e4d33fe00a3429f
吉富　海 <tky2202173@stu.o-hara.ac.jp>
2024-01-18 13:44:37 +0900

    認証用model,admin

--- app_album/admin.py
+++ app_album/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import School, Student, Teacher, Album
+from .models import School, Student, Teacher, Album, Profile_student, Profile_teacher
 
 
 class SchoolAdmin(admin.ModelAdmin):
@@ -12,24 +12,40 @@ class AlbumAdmin(admin.ModelAdmin):
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = [
-        "school",
         "mail_address",
-        "password",
         "student_id",
         "student_name",
-        "number",
-        "class_name",
-        "comment",
+        "password",
     ]
 
 
 class TeacherAdmin(admin.ModelAdmin):
     list_display = [
-        "school",
         "mail_address",
         "password",
         "teacher_id",
         "teacher_name",
+    ]
+
+
+class Profile_studentAdmin(admin.ModelAdmin):
+    list_display = [
+        "profile_student_id",
+        "school",
+        "album",
+        "student",
+        "class_name",
+        "class_number",
+        "comment",
+    ]
+
+
+class Profile_teacherAdmin(admin.ModelAdmin):
+    list_display = [
+        "profile_teacher_id",
+        "school",
+        "album",
+        "teacher",
         "comment",
     ]
 
@@ -38,3 +54,5 @@ admin.site.register(School, SchoolAdmin)
 admin.site.register(Album, AlbumAdmin)
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Profile_student, Profile_studentAdmin)
+admin.site.register(Profile_teacher, Profile_teacherAdmin)
--- app_album/models.py
+++ app_album/models.py
@@ -13,13 +13,9 @@ class Album(models.Model):
 
 
 class Student(models.Model):
-    school = models.ForeignKey(School, on_delete=models.CASCADE)
     mail_address = models.CharField(max_length=100, null=False)
     student_id = models.AutoField(primary_key=True)
     student_name = models.CharField(max_length=50, null=False)
-    number = models.IntegerField(null=False)
-    class_name = models.CharField(max_length=20, null=False)
-    comment = models.CharField(max_length=100, null=False)
     password = models.CharField(max_length=128, null=False)
 
     def save(self, *args, **kwargs):
@@ -29,14 +25,30 @@ class Student(models.Model):
 
 
 class Teacher(models.Model):
-    school = models.ForeignKey(School, on_delete=models.CASCADE)
     mail_address = models.CharField(max_length=100, null=False)
     teacher_id = models.AutoField(primary_key=True)
     teacher_name = models.CharField(max_length=50, null=False)
-    comment = models.CharField(max_length=100, null=False)
     password = models.CharField(max_length=128, null=False)
 
     def save(self, *args, **kwargs):
         # パスワードをハッシュ化して保存
         self.password = make_password(self.password)
         super().save(*args, **kwargs)
+
+
+class Profile_student(models.Model):
+    profile_student_id = models.AutoField(primary_key=True)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    class_name = models.CharField(max_length=20, null=False)
+    class_number = models.IntegerField(null=False)
+    comment = models.CharField(max_length=100, null=False)
+
+
+class Profile_teacher(models.Model):
+    profile_teacher_id = models.AutoField(primary_key=True)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=100, null=False)
